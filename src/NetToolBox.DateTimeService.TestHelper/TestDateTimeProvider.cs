25a670dd5da95ba949eaf1d3534d43754a0cb072
Nicholas P Nelson <nnelson1@insight.rr.com>
2020-05-08 19:10:04 -0400

    Initial Version (#1)

---
+++ src/NetToolBox.DateTimeService.TestHelper/TestDateTimeProvider.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace NetToolBox.DateTimeService.TestHelper
+{
+    public sealed class TestDateTimeServiceProvider : IDateTimeService
+    {
+
+
+        private DateTime? _currentDateTime;
+
+        /// <summary>
+        /// Sets the CurrentDateTime for the IDateTimeProvider for testing purposes
+        /// </summary>
+        /// <param name="dateTime"></param>
+        public void SetCurrentDateTimeUTC(DateTime dateTime)
+        {
+            _currentDateTime = dateTime;
+        }
+
+        /// <summary>
+        /// Gets the CurrentDateTime.  Initializes upon first call to then current datetime or can be set explicitly through SetCurrentDateTime
+        /// Once CurrentDateTime is set, you must call SetCurrentDateTime to change it, it will not automatically update on its own.
+        /// </summary>
+        /// <returns></returns>
+        public DateTime CurrentDateTimeUTC
+        {
+            get
+            {
+                if (_currentDateTime == null) //if it hasn't been set, set it to current date time
+                {
+                    //to eliminate some precision problems when using datetime vs datetime2 in SQL, we will take the current time and chop the milliseconds off
+                    var currentDate = DateTime.Now;
+                    _currentDateTime = new DateTime(currentDate.Year, currentDate.Month, currentDate.Day, currentDate.Hour,
+                    currentDate.Minute, currentDate.Second);
+                }
+                return _currentDateTime.Value;
+            }
+        }
+
+        /// <summary>
+        /// Adds seconds to the current DateTime value. 
+        /// If CurrentDateTime is null it sets it to the DateTime.Now and then adds the seconds
+        /// </summary>
+        /// <param name="seconds"></param>
+        public void AdvanceSeconds(int seconds)
+        {
+            if (_currentDateTime == null) //if it hasn't been set, set it to current date time
+            {
+                _currentDateTime = CurrentDateTimeUTC;
+            }
+            _currentDateTime = _currentDateTime.Value.AddSeconds(seconds);
+        }
+
+        /// <summary>
+        /// Adds seconds to the current DateTime value. 
+        /// If CurrentDateTime is null it sets it to the DateTime.Now and then adds the minutes
+        /// </summary>
+        /// <param name="minutes"></param>
+        public void AdvanceMinutes(int minutes)
+        {
+            if (_currentDateTime == null) //if it hasn't been set, set it to current date time
+            {
+                _currentDateTime = CurrentDateTimeUTC;
+            }
+            _currentDateTime = _currentDateTime.Value.AddMinutes(minutes);
+        }
+
+        /// <summary>
+        /// Adds seconds to the current DateTime value. 
+        /// If CurrentDateTime is null it sets it to the DateTime.Now and then adds the days
+        /// </summary>
+        /// <param name="days"></param>
+        public void AdvanceDays(int days)
+        {
+            if (_currentDateTime == null) //if it hasn't been set, set it to current date time
+            {
+                _currentDateTime = CurrentDateTimeUTC;
+            }
+            _currentDateTime = _currentDateTime.Value.AddDays(days);
+        }
+
+    }
+}
